bb2d3a1146d2a065fe709a39a833a0c4bbe0f32f
Genevieve Conty <contygm@gmail.com>
2018-11-07 15:45:57 -0800

    week7 7b person class works as expected

--- week7/7b/main.cpp
+++ week7/7b/main.cpp
@@ -1,6 +1,22 @@
-// Person
-    // two data members: name (str), age (double)
-    // initalize contructor
-    // get name
-    // get age
-    // no sets
+#include <iostream>
+#include "Person.hpp"
+using std::cout;
+using std::endl;
+
+int main() 
+{
+    // initialize
+    Person jay("Jay", 29);
+    Person steve("Steve", 33);
+    Person you("You", 26);
+
+    // check initialization and getters work
+    cout << you.getName() << " are " << you.getAge() << endl;
+    cout << jay.getName() << " is " << jay.getAge() << endl;
+    cout << steve.getName() << " is " << steve.getAge() << endl;
+
+    // TODO: check std deviation is as expected
+
+
+    return 0;
+}
--- week7/7b/stdDev.cpp
+++ week7/7b/stdDev.cpp
@@ -3,3 +3,10 @@
     // returns standard deviation of all ages
 
 // squareRoot ( sum( (X_i - avg)^2) ) / N ) 
+
+int stdDev()
+{
+    int stdDev = 0;
+
+    return stdDev;
+}
