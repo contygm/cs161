08b638bfdaa01d2fe28033ca71d8f1f9ff8f1575
Genevieve Conty <contygm@gmail.com>
2018-11-07 16:26:11 -0800

    7b stdDev works as expected

--- week7/7b/main.cpp
+++ week7/7b/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "Person.hpp"
+#include "stdDev.cpp"
 using std::cout;
 using std::endl;
 
@@ -9,24 +10,24 @@ int main()
     Person you("You", 26.5);
     Person jay("Jay", 29.75);
     Person steve("Steve", 33);
-    // Person elder("Elder", 109);
-    // Person child("child", 7);
-    // Person baby("Baby", 0.5);
+    Person elder("Elder", 109);
+    Person child("child", -1);
+    Person baby("Baby", 0.5);
 
     // check initialization and getters work
     cout << you.getName() << " are " << you.getAge() << endl; // 26.5
     cout << jay.getName() << " is " << jay.getAge() << endl; // 29.75
     cout << steve.getName() << " is " << steve.getAge() << endl; // 33
-    // cout << elder.getName() << " is " << elder.getAge() << endl; // 109
-    // cout << child.getName() << " is " << child.getAge() << endl; // 7
-    // cout << baby.getName() << " is " << baby.getAge() << endl; // 0.5
+    cout << elder.getName() << " is " << elder.getAge() << endl; // 109
+    cout << child.getName() << " is " << child.getAge() << endl; // 7
+    cout << baby.getName() << " is " << baby.getAge() << endl; // 0.5
 
-    Person peeps[3] = {you, jay, steve}
-    // TODO: check std deviation is as expected
-    cout << "Standard Deviation 0 is " << stdDeviation(peeps, 3) << endl; // 2.6536138880151
+    // check std deviation is as expected
+    Person peeps[3] = {you, jay, steve};
+    stdDev(peeps, 3); // 2.6536138880151
 
-    // Person peeps1[6] = {you, jay, steve, elder, child, baby}
-    // cout << "Standard Deviation 1 is " << stdDeviation(peeps1, 6) << endl; // 35.475465896244
+    Person peeps1[6] = {you, jay, steve, elder, child, baby};
+    stdDev(peeps1, 6); // 35.475465896244
 
 
     return 0;
--- week7/7b/stdDev.cpp
+++ week7/7b/stdDev.cpp
@@ -1,12 +1,46 @@
-// function
-    // two params: array of Persons, size of array
-    // returns standard deviation of all ages
 
-// squareRoot ( sum( (X_i - avg)^2) ) / N ) 
+#include <iostream>
+#include <cmath>
+#include "Person.hpp"
 
-int stdDev()
+// REMOVE
+using std::cout;
+using std::endl;
+
+double stdDev(Person people[], int size)
 {
-    int stdDev = 0;
+    double stdDev = 0;
+    double ages[size];
+    double sumAge = 0;
+    double averageAge = 0;
+    double sumDiff = 0;
+
+    for (int i = 0; i < size; i++)
+    {
+        double personAge = people[i].getAge();
+        cout << "personAge: " << personAge << endl;
+
+        ages[i] = personAge;
+        sumAge += personAge;
+    }
+
+    cout << "sumAge: " << sumAge << endl;
+
+    averageAge = ( sumAge / size );
+
+    cout << "averageAge: " << averageAge << endl;
+
+    
+    for (int i = 0; i < size; i++)
+    {
+        double diff = ages[i] - averageAge;
+        
+        sumDiff += pow(diff, 2);
+    }
+
+    stdDev = sqrt(sumDiff / size); 
+    // squareRoot ( sum( (X_i - avg)^2) ) / N ) 
+    cout << "Standard Deviation is " << stdDev << endl; // 2.6536138880151
 
     return stdDev;
 }
