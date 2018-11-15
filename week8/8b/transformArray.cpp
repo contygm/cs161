690c9d71a40df159dcc25de492552ba596b91ebd
Genevieve Conty <contygm@gmail.com>
2018-11-14 18:15:55 -0800

    some progress

--- week8/8b/transformArray.cpp
+++ week8/8b/transformArray.cpp
@@ -11,23 +11,23 @@ using std::cout;
 void transformArray(int* array, int size)
 {
     // dynamically allocate an array that is twice as long, 
-    // int transformedArray = new int[size*2];
+    int* transformedArray = new int[size*2];
 
     // fill with the values from the original array 
     // then after, fill remaining space with each of those values plus one
-    // for (int i=0; i < (size); i++)
-    // {
-    //     transformArray[i] = array[i];
-    //     transformArray[size+i] = array[i]+1
-    // }
+    for (int i=0; i < size; i++)
+    {
+        transformArray[i] = *(array+i);
+        transformArray[size+i] = *(array+i)+1;
+    }
         
-    // for (int i=size; i < (size*2); i++)
-    //     cout << transformArray[i] << endl;
+    for (int i=size; i < (size*2); i++)
+        cout << transformArray[i] << endl;
     
-    for (int i=size; i < (size); i++)
-        cout << *array[i] << endl;
+    // for (int i=0; i < size; i++)
+    //     cout << "YO" << *(array+i) << endl;
     
     // array = transformedArray;
 
-    // delete []transformedArray;
+    delete []transformedArray;
 }
